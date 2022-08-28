4e56d97c0934590b2b30ebcceff62a67933ee9f8
Grace Durant <gdurant34@gmail.com>
2022-08-28 00:09:03 -0700

    added content to website

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const navigationBar = document.createElement("div");
+navigationBar.className = "birdseye-header";
+document.querySelector("body").append("navigationBar");
